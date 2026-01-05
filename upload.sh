1f7b010e0e41ac18d04cfac445a7af777f0d52f3
Damian-Oswald <damian.oswald@protonmail.com>
2026-01-05 16:55:12 +0100

    Move password etc. to .env file

---
+++ upload.sh
@@ -0,0 +1,25 @@
+# load environment variables
+. ./.env
+
+# Run the Python script
+echo "Running Python reasoning script..."
+python3 scripts/reason.py
+
+echo "Delete existing data from LINDAS"
+curl \
+  --user $USER:$PASSWORD \
+  -X DELETE \
+  "$ENDPOINT?graph=$GRAPH"
+
+echo "Upload graph.ttl file to LINDAS"
+curl \
+  --user $USER:$PASSWORD \
+  -X POST \
+  -H "Content-Type: text/turtle" \
+  --data-binary @rdf/graph.ttl \
+  "$ENDPOINT?graph=$GRAPH"
+
+echo "Remove graph.ttl file"
+rm rdf/graph.ttl
+
+echo "All commands executed."

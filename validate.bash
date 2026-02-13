4e2a4c8362c8782665b313e5acf4c100e88009c5
Damian-Oswald <damian.oswald@protonmail.com>
2026-02-13 09:22:11 +0100

    Split old/new .env files; hide via .gitignore

--- validate.bash
+++
@@ -1,6 +0,0 @@
-# Run the Python/SPARQL reasoning script
-echo "Running Python reasoning script..."
-python3 scripts/reason.py
-
-# Apply SHACL rules to check for constraint violations
-pyshacl -s rdf/shape.ttl -f human rdf/graph.ttl
